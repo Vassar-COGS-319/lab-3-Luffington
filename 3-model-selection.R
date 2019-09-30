bf63e61c4882b203eefffcc4d72de94caa5cfcc7
Luffington <ddowie@vassar.edu>
2019-09-29 23:42:28 -0400

    Update 3-model-selection.R
    Attempt at lab 3 part 3, absolutely must be revisited

--- 3-model-selection.R
+++ 3-model-selection.R
@@ -15,6 +15,78 @@
 # You don't need to get a perfect match. Just get in the ballpark. 
 
 
+#random-walk-model code(edited):
+
+drift <- 0
+sdrw <- 0.3
+criterion <- 3
+internal_evidence_interval <- 0
+#sample a null distribution and assign to new tag
+compare.to.criterion <- sample(rnorm(drift, sdrw))
+#if the compare.to.criterion value is less than the criterion, we want the function to continue running,
+#so we check them against each other
+while ((compare.to.criterion < criterion)||(compare.to.criterion > -criterion)){
+  #add the value to the internal evidence interval 
+  compare.to.criterion <- sum(z, internal.evidence.interval)
+  #this keeps track of how many interations we've run through up to this point
+  sum(internal_evidence_interval, 1)
+  #If our compare value exceeds the criterion,m we want to break out of the while loop and return 
+  #the number of trials it took to get here
+  else 
+    return(internal_evidence_interval)
+  
+}
+
+#The number of tests should be the same as the number of iterations it took for the criterion to be met
+initial.test <- random.walk.model(internal_evidence_interval)
+sum(initial.test$correct) / length(initial.test$correct) # should be close to 0.5
+mean(initial.test$rt) # should be about 112
+
+#accumulator-model code (edited):
+
+#I included the average response speeds as substitute values for the rates from the second model with the reasoning that 
+#they represent the rate of response to both correct and incorrect responses. The criterion has been reset to
+#0.8 because this is the accuracy level that must be acheived to maintain consistency between the model and the
+#actual result. 
+
+accumulator.model <- function(samples, rate.1=250, rate.2=246, criterion=0.8){
+  
+  ##established a counter to keep track of the iterations
+  ##most of the rest of this code can be repurposed with the new values?
+  
+  iterations <- 0
+  #accumulators to store the random values sampled from the exponental distribution
+  evidence.accumulator(rate.1) <- 0
+  evidence.accumulator(rate.2) <- 0
+  #start a while loop that terminates when the threshold value is reaached
+  while(evidence.accumulator < criterion){
+    #sampling random values from an exponential distribution of the rates we set in the accumulator function
+    random_values <- sample(rexp(rate.1, rate.2))
+    #add the sampled value from each rate to the evidence accumulators using the sum function
+    sum(evidence.accumulator(rate.1), random_values)
+    sum(evidence.accumulator(rate.2), random_values)
+    #add one to the iterations counter so we can see how many cycles it takes to reach the criterion value
+    sum(iterations, 1)
+    else
+      return(output)
+    print(iterations)
+    
+    rt <- iterations
+    correct <- c(evidence.accumulator(rate.1), evidence.accumulator(rate2))
+    output <- data.frame(
+      correct = accuracy.array,
+      rt = rt.array
+    )
+  }
+  return(output)
+}
+
+#Number of trails edited for the same reasons as in the model above
+initial.test <- accumulator.model(iterations)
+sum(initial.test$correct) / length(initial.test$correct) # should be close to 0.5
+mean(initial.test$rt) # should be about 112
+
+
 # Can both models do a reasonable job of accounting for the mean RT and accuracy? Report the
 # results of your efforts:
 
